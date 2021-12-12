cd81a60132871c380871fa7c7bfe786d20a49247
Ethan Gallant <ethan@ziax.com>
2021-12-12 14:05:19 -0400

    add dotfiles

---
+++ profile/.config/bin/xrandr_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xrandr	--output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+	--output DP-5 --mode 1920x1080 --pos 1920x0 --rotate normal \
+	--output DP-1 --mode 1920x1080 --pos 3840x0 --rotate normal
